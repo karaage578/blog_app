55da91efbbe873f12b1feda4b6c7b3777f01f7b9
masanori.takeda <karaage118@gmail.com>
2019-04-10 18:50:22 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/" => "posts#index"
-  get "/posts/show" => "posts#show"
+  get "/posts/:id" => "posts#show"
   get "/posts/new" => "posts#new"
   post "/posts/create" => "posts#create"
 end
