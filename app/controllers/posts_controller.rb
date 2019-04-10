5dc61e80bbf6961b386cb0304dc56c1d8ef9adad
masanori.takeda <karaage118@gmail.com>
2019-04-10 18:30:37 +0900

    新規投稿、一覧詳細ページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+    def new
+      @post = Post.new
+    end
+    
+    def create
+      @post = Post.new(post_params)
+      @post.save
+      flash[:notice]="作成しました"
+      redirect_to("posts/index")
+    end
+
+    def index
+      @posts = Post.all.order(created_at: :desc)
+    end
+
+    def show
+      @post = Post.find(params[:id])
+    end
+
+    private
+    
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
